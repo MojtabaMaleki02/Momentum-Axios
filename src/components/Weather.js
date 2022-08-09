38dd3806c55fcd974c1a724c5860c6173ad4690c
Mojtaba Maleki <mojtaba.maleki.138022@gmail.com>
2022-08-09 16:30:20 +0200

    Axios :)

---
+++ src/components/Weather.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import {WeatherContainer, Temp, City } from './WeatherStyles'
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=60db43332ab5bf8e954f6254102765cf'
+
+const Weather = () => {
+    const [data, setData] = useState(null)
+
+    useEffect(() =>{
+        axios.get(url).then((response) => {
+            setData(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    },[])
+
+    console.log(data)
+
+    if(!data) return null;
+
+  return (
+    <WeatherContainer>
+      <Temp>{data.main.temp.toFixed(0)}&#176;</Temp>
+      <City>Budapest, HU</City>
+    </WeatherContainer>
+
+  )
+}
+
+export default Weather
